c763289b0af2d1a9cba8739896d5e56f24ac1594
OnE-113 <nlsuther@mtu.edu>
2024-10-04 21:07:15 -0400

    Added more to things
    Jump is functional, and mouse disappears when game is clicked.

--- Midnights Dreary/Assets/Mini Game/Scripts/Movement.cs
+++ Midnights Dreary/Assets/Mini Game/Scripts/Movement.cs
@@ -8,8 +8,21 @@ public class Movement : MonoBehaviour
     public CharacterController characterController;
     public Transform jumpCheck;
     public Transform cam;
+    public LayerMask groundMask;
 
     public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public float groundDistance = 0.4f;
+    public float turnTime = 0.5f;
+    bool isGrounded;
+    float turnVelocity;
+    Vector3 velocity;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,16 +33,25 @@ public class Movement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f) { 
             float targetAngle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            //float angle = 
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             characterController.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
-        if(Input.GetButtonDown("Jump")){
+        isGrounded = Physics.CheckSphere(jumpCheck.position, groundDistance, groundMask);
+        velocity.y += gravity * Time.deltaTime;
 
+        if(Input.GetButtonDown("Jump") && isGrounded){
+                velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2;
+        }
+
+        characterController.Move(velocity * Time.deltaTime);
 
     }
 }
