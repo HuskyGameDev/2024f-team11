6d9fcd285e89fa46b59e4fc2ba5e8e41a82fca3f
eblappen <eblappen@mtu.edu>
2024-10-24 13:25:17 -0400

    Reworked pickup system to originate from the camera
    It used to cast a ray from the player's position forward which caused issues when trying to pick up items from a slope. Now the ray is cast from the camera's position and rotation to reliably pick up objects the player is looking at.

--- Seeking Solace/Assets/MainGame/Scripts/PickUpDrop.cs
+++ Seeking Solace/Assets/MainGame/Scripts/PickUpDrop.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PickupDrop : MonoBehaviour
 {
-    public Transform player;           // Reference to the player or camera transform
+    public Transform camera;           // Reference to the player or camera transform
     public float pickupRange = 3f;     // How close the player needs to be to pick up the item
     public Transform itemHoldPosition; // The position where the item will be held
     private GameObject pickedUpItem;   // The currently picked up item
@@ -28,8 +28,9 @@ public class PickupDrop : MonoBehaviour
     void TryPickUpItem()
     {
         RaycastHit hit;
+
         // Cast a ray from the player's position to detect objects in front of them
-        if (Physics.Raycast(player.position, player.forward, out hit, pickupRange))
+        if (Physics.Raycast(camera.position, camera.forward, out hit, pickupRange))
         {
             // Check if the object is "pickable" by having a tag or specific layer
             if (hit.collider.CompareTag("Pickable"))
@@ -41,6 +42,7 @@ public class PickupDrop : MonoBehaviour
                 {
                     itemRb.isKinematic = true; // Disable physics while holding the object
                     pickedUpItem.transform.position = itemHoldPosition.position;
+                    pickedUpItem.transform.rotation = itemHoldPosition.rotation;
                     pickedUpItem.transform.SetParent(itemHoldPosition);
                 }
             }
