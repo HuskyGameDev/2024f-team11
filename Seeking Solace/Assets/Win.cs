c56a89476c40e1d09ecb05982fb0c4ea3376b776
JesterlyKing <iswashin@mtu.edu>
2024-11-19 17:21:47 -0500

    Mini Game game over and Win screens along with objective

---
+++ Seeking Solace/Assets/Win.cs
@@ -0,0 +1,25 @@
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Win : MonoBehaviour
+{
+    public GameObject popupUI; // Assign your UI Panel here in the Inspector
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the player collided
+        if (other.CompareTag("Player"))
+        {
+            // Show the popup
+            if (popupUI != null)
+            {
+                popupUI.SetActive(true);
+            }
+        }
+    }
+}
+
