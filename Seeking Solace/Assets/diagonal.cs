30f83d4e1f6eb0f0bd019cf698db6f391e74bf6e
JesterlyKing <iswashin@mtu.edu>
2025-03-17 18:29:33 -0400

    Level 2 Start
    Began making a base level 2

---
+++ Seeking Solace/Assets/diagonal.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class diagonal : MonoBehaviour
+{
+    public float speed = 3f;          // Movement speed
+    public float distance = 5f;       // Distance to move from starting point
+
+    private Vector3 startPos;
+    private bool movingRight = true;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // Move enemy side to side
+        float move = speed * Time.deltaTime * (movingRight ? 1 : -1);
+        transform.Translate(move, move, 0);
+
+        // Check distance and reverse direction
+        if (Vector3.Distance(startPos, transform.position) >= distance)
+            movingRight = !movingRight;
+    }
+}
